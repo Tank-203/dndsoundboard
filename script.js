21f5d2f4c635ddc51f3f2a8679380c0cbdc343a6
Ethan <ethan.stucker@outlook.com>
2022-05-06 00:27:03 -0500

    Hope

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function hello() {
-    alert("Hello");
+const sounds = ['Vine-Boom']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+    
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).onplay()
+    })
+
+    document.getElementById('buttons').appendChildapp(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
 }
